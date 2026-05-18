d0c6d52af968191bda79f854aa5f6fa591a177a9
Sujal581 <palsujal869@gmail.com>
2026-05-18 17:35:11 +0530

    Update 6_🔬_Data_Explorer.py

--- pages/6_🔬_Data_Explorer.py
+++ pages/6_🔬_Data_Explorer.py
@@ -5,7 +5,7 @@ from style import (inject_css, sidebar_brand, page_header, section_header,
                    kpi_card, chart_label, insight_card, apply_plot_layout, footer, COLORS, COLOR_SEQ)
 from data_manager import apply_filters
 
-st.set_page_config(page_title="Data Explorer | Nassau Candy", layout="wide", page_icon="🔬")
+st.set_page_config(page_title="Data Explorer | Nassau Candy", layout="wide", page_icon="🔬",initial_sidebar_state="expanded")
 inject_css()
 sidebar_brand()
 
